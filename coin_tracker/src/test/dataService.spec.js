49ba249cef1a5eb773c45bae82641cbe2d2e6fc6
Jun Kim <93kimhyunjun@gmail.com>
2019-12-21 23:06:00 +0900

    테스트 코드 추가하다가 실패

--- coin_tracker/src/test/dataService.spec.js
+++ coin_tracker/src/test/dataService.spec.js
@@ -1,3 +1,18 @@
-import dataService from '../dataService'
+const dataService = require('../dataService')
 
-describe('dataService의', () => {})
+describe('dataService의', () => {
+  let ds
+  beforeEach(() => (ds = dataService()))
+
+  describe('ds.sortOutDataRank는', () => {
+    it('랭크가 높은순으로 데이터를 정렬한다.', () => {
+      const stubArr = [{ rank: 2 }, { rank: 3 }, { rank: 1 }]
+      const expected = [{ rank: 1 }, { rank: 2 }, { rank: 3 }]
+
+      ds.sortOutDataRank(stubArr)
+      expect(stubArr).toStrictEqual(expected)
+    })
+  })
+
+  describe('ds.')
+})
